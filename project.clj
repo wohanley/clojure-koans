16eb76dad205c5edf4d445c61a52b5e4d5dfce5b
William O'Hanley <liam@wohanley.com>
2014-10-03 21:21:54 +1300

    add CIDER to project

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [koan-engine "0.2.3"]]
   :dev-dependencies [[lein-koan "0.1.3"]]
-  :profiles {:dev {:dependencies [[lein-koan "0.1.3"]]}}
+  :profiles {:dev {:dependencies [[lein-koan "0.1.3"]]
+                   :plugins [[cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
   :repl-options {:init-ns koan-engine.runner
                  :init (do (use '[koan-engine.core]))}
   :plugins [[lein-koan "0.1.3"]]
